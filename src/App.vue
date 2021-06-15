ade9c688f915873bb583c7932cd60c28d7e6060a
이효진 <billion109@naver.com>
2021-06-15 19:28:57 +0900

    Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <todo-header></todo-header>
+      <todo-input v-on:addTodo="addTodo"></todo-input>
+      <todo-list v-bind:propsdata="todoItems" @removeTodo="removeTodo"></todo-list>
+      <todo-footer v-on:removeAll="clearAll"></todo-footer>
+      
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoHeader from './components/TodoHeader.vue'
+import TodoFooter from './components/TodoFooter.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components:{
+        'TodoHeader':TodoHeader,
+        'TodoFooter':TodoFooter,
+        'TodoInput':TodoInput,
+        'TodoList':TodoList
+    },
+    data(){
+        return{
+            todoItems:[]
+        }
+    },
+    methods:{
+        addTodo(todoItem){
+            localStorage.setItem(todoItem, todoItem);
+            this.todoItems.push(todoItem);
+        },
+        clearAll(){
+            localStorage.clear();
+            this.todoItems = [];
+        },
+        removeTodo(todoItem, index){
+            localStorage.removeItem(todoItem);
+            this.todoItems.splice(index,1);
+        }
+    },
+    created(){
+        if (localStorage.length>0){
+            for (var i =0; i<localStorage.length; i++){
+                this.todoItems.push(localStorage.key(i));
+            }
+        }
+    },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+    text-align: center;
+    background-color: #F6F6F8;
+}
+input{
+    border-style: groove;
+    width:200px;
+}
+button{
+    border-style: groove;
+}
+.shadow{
+    box-shadow: 5px 10px 10px rgba(0,0,0,0.03);
 }
 </style>
